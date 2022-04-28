778e34b232424ff6a38d86c66b08bbd0ef1a6685
Sayaka <j.sadolf1123@gmail.com>
2022-04-28 20:33:09 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\TodoController;
 Route::get('/',[TodoController::class,'index']);
 Route::get('/todo/create',[TodoController::class,'create']);
 Route::post('/todo/create', [TodoController::class,'create'])
+//Route::get('/todo/update', [TodoController::class,'update'])
 //Route::post('/todo/update', [TodoController::class,'update'])
 //Route::get('/todo/delete',[TodoController::class,'delete']);
 //Route::post('/todo/delete', [TodoController::class,'delete'])
