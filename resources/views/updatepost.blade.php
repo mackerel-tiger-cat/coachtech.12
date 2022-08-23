739f25352918d9d8d8336b8c25d01c8951656f69
Sayaka <j.sadolf1123@gmail.com>
2022-08-23 20:24:27 +0900

    update

---
+++ resources/views/updatepost.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>COACHTECH</title>
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+</head>
+<body>
+  <h1>{{$input}}</h1>
+</body>
+</html>
+</body>
+</html>
